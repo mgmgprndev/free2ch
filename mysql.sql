df44594485f7750b8f2726719270d00d2c7349e7
もぐもぐぷりん <mgmgprndev@gmail.com>
2024-07-20 02:46:25 +0900

    Update mysql.sql

--- mysql.sql
+++ mysql.sql
@@ -1,9 +1,35 @@
+-- idk if this working :p
 SET time_zone = "+09:00";
 
+-- FOR USER CREATION, MAYBE YOU CAN CHECK SOME TUTORIALS.
+-- I WILL RECOMMEND "Sequel Pro" TO MANAGE MYSQL DATABASES. It useful!
+-- HOW TO INSTALL MYSQL(MARIADB)
+-- sudo apt install mariadb-server -y
+-- mysql_secure_installation
+-- mysql to login to database...
+-- Then create your account on database :
+-- 
+-- CREATE USER 'username'@'%' IDENTIFIED BY 'password';
+-- CREATE DATABASE database_name;
+-- GRANT ALL PRIVILEGES ON database_name.* TO 'username'@'%';
+-- FLUSH PRIVILEGES;
+--
+-- IF YOU WANNA ALLOW ACCESS FROM OUTSIDE OF SERVER, LET BIND TO 0.0.0.0!
+-- sudo nano /etc/mysql/mariadb.conf.d/50-server.cnf
+-- maybe the file dir difference by OS... idk!
+-- Find line "bind-address" and change value to "0.0.0.0"
+
+
+
+-- "Drop table" means delete the table itself and the datas inside of it.
+-- So please remove it if you are not intent to it.
 DROP TABLE IF EXISTS verifys;
 DROP TABLE IF EXISTS boards;
 DROP TABLE IF EXISTS threads;
 DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS admins;
+
+--  BELOWS ARE CREATION OF TABLES!
 
 CREATE TABLE IF NOT EXISTS verifys (
     id INT PRIMARY KEY AUTO_INCREMENT,
